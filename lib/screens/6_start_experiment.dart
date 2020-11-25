f75d339e8d890daeeb2377a01332e13ef09cb7bb
Luca Allegro <luca.allegro.3@studenti.unipd.it>
2020-11-25 23:36:59 +0100

    Improved 'start experiment' screen

--- lib/screens/6_start_experiment.dart
+++ lib/screens/6_start_experiment.dart
@@ -14,27 +14,48 @@ class StartExperiment extends StatelessWidget {
       onWillPop: () async => false,
       child: SafeArea(
         child: Scaffold(
-          body: Column(children: [
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
-              width: double.infinity,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    'Complimenti!',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                  Text(
-                    'Ora avrà inizio l\'esperimento',
-                    style: Theme.of(context).textTheme.bodyText2,
-                  )
-                ],
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Complimenti!',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    Container(
+                        padding: EdgeInsets.only(top: 10),
+                        width: double
+                            .infinity // used to force the column to expand to full width
+                        ),
+                    Text(
+                      'Ora avrà inizio l\'esperimento',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ],
+                ),
               ),
-            )
-          ]),
+              Expanded(
+                flex: 2,
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxWidth: 200,
+                    maxHeight: 200,
+                  ),
+                  child: Image.asset(
+                    'images/start_experiment.png',
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+              ),
+            ],
+          ),
           bottomNavigationBar: BottomButton(
             text: "Avanti",
             onPressed: () {
